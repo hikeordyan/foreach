f1140af521074c72ba3b9a9977e241441976e3f8
haykordyan <hike.ordyan@gmail.com>
2015-10-12 18:37:05 +0400

    df

---
+++ foreach/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace @foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] fibarray = new int[] { 0, 1, 1, 2, 3, 5, 8, 13 };
+             foreach (int element in fibarray)
+             {
+                System.Console.WriteLine(element);
+             }
+
+        }
+    }
+}
